6e901f8276e993f9ae48e3e2995fbbc623ae5189
organroman <99799516+organroman@users.noreply.github.com>
2022-02-17 00:53:16 +0200

    Delete src directory

--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-(function handleBurger () {
-    document.querySelector('.burger').addEventListener('click', ()=>{
-        document.querySelector('.menu').classList.toggle('active')
-        document.querySelector('.burger').classList.toggle('close')
-    })
-})()
